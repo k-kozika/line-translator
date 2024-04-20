746e1b252767f6bea4c79b31c2f797b835d82aa9
k-kozika <127573443+k-kozika@users.noreply.github.com>
2024-04-20 19:45:58 +0900

    fix some bug

--- src/handlers/groupGreet.ts
+++ src/handlers/groupGreet.ts
@@ -4,49 +4,45 @@ import { countMembersInGroup, leaveGroup, reply } from "../lib/line";
 export const groupGreet: Handler = async (event, next) => {
   if (event.type !== "join" || event.source.type !== "group") return next();
 
-  const count = countMembersInGroup(
-    event.source.groupId
-  );
+  const count = countMembersInGroup(event.source.groupId);
 
   if (count <= 1) {
     reply(event.replyToken, [
       {
         type: "flex",
-        contents: [
-          {
-            type: "bubble",
-            body: {
-              type: "box",
-              layout: "vertical",
-              contents: [
-                {
-                  type: "text",
-                  text: "グループへの追加ありがとうございます",
-                  wrap: true,
+        contents: {
+          type: "bubble",
+          body: {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "text",
+                text: "グループへの追加ありがとうございます",
+                wrap: true,
+              },
+              {
+                type: "text",
+                text: "しかし、メンバーが少なすぎるためグループを作成する必要はないようです",
+                wrap: true,
+              },
+              {
+                type: "text",
+                text: "チャットをご利用ください",
+                wrap: true,
+              },
+              {
+                type: "button",
+                action: {
+                  type: "uri",
+                  label: "開く",
+                  uri: "https://line.me/R/oaMessage/%40982qzrdh",
                 },
-                {
-                  type: "text",
-                  text: "しかし、メンバーが少なすぎるためグループを作成する必要はないようです",
-                  wrap: true,
-                },
-                {
-                  type: "text",
-                  text: "チャットをご利用ください",
-                  wrap: true,
-                },
-                {
-                  type: "button",
-                  action: {
-                    type: "uri",
-                    label: "開く",
-                    uri: "https://line.me/R/oaMessage/%40982qzrdh",
-                  },
-                  style: "primary",
-                },
-              ],
-            },
+                style: "primary",
+              },
+            ],
           },
-        ],
+        },
         altText: "グループは必要ありません",
       },
     ]);
--- src/handlers/groupNotice.ts
+++ src/handlers/groupNotice.ts
@@ -1,5 +1,5 @@
 import type { Handler } from ".";
-import {  reply } from "../lib/line";
+import { reply } from "../lib/line";
 
 export const groupNotice: Handler = async (event, next) => {
   next();
@@ -8,34 +8,33 @@ export const groupNotice: Handler = async (event, next) => {
   reply(event.replyToken, [
     {
       type: "flex",
-      contents: [
-        {
-          type: "bubble",
-          body: {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "text",
-                size: "lg",
-                weight: "bold",
-                text: "参加された方へのご案内",
-              },
-              {
-                type: "text",
-                text: "このグループでは自動翻訳が行われています",
-                wrap: true,
-              },
-              {
-                type: "text",
-                text: "Google翻訳に送信したメッセージが共有されます",
-                wrap: true,
-              },
-            ],
-            spacing: "md",
-          },
+      contents: {
+        type: "bubble",
+        body: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              size: "lg",
+              weight: "bold",
+              text: "参加された方へのご案内",
+            },
+            {
+              type: "text",
+              text: "このグループでは自動翻訳が行われています",
+              wrap: true,
+            },
+            {
+              type: "text",
+              text: "Google翻訳に送信したメッセージが共有されます",
+              wrap: true,
+            },
+          ],
+          spacing: "md",
         },
-      ],
+      },
+      altText: "はじめにご確認ください"
     },
   ]);
 };
